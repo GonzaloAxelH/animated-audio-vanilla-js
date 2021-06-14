4b37bc8317e2e12e9cea6b684871ae401e347f5f
Gonzalo-Code-Pro <yubasyubas123@gmail.com>
2021-06-13 22:05:48 -0500

    add to funcions

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,49 @@
 $(document).ready(() => {
+
+
+
+
+
+
+
+    var context = new AudioContext({ sampleRate: 44100 });
+    var fileInput = document.getElementById("fileInput");
+
+    fileInput.onchange = function () {
+        var files = fileInput.files;
+
+        if (files.length == 0) return;
+        var reader = new FileReader();
+
+        reader.onload = function (fileEvent) {
+            context.decodeAudioData(fileEvent.target.result, calcTempo);
+        }
+
+        reader.readAsArrayBuffer(files[0]);
+    }
+    var calcTempo = function (buffer) {
+        var audioData = [];
+        // Take the average of the two channels
+        if (buffer.numberOfChannels == 2) {
+            var channel1Data = buffer.getChannelData(0);
+            var channel2Data = buffer.getChannelData(1);
+            var length = channel1Data.length;
+            for (var i = 0; i < length; i++) {
+                audioData[i] = (channel1Data[i] + channel2Data[i]) / 2;
+            }
+        } else {
+            audioData = buffer.getChannelData(0);
+        }
+        var mt = new MusicTempo(audioData);
+
+        console.log(mt.tempo);
+        console.log(mt.beats);
+    }
+
+
+
+
+    //player
     $('.player__spinner-wrap').click(function () {
         var $this = $(this),
             audio = $this.siblings('audio')[0]
@@ -7,13 +52,15 @@ $(document).ready(() => {
         console.log(pulse)
 
         if (audio.paused == false) {
+
             audio.pause();
             audio.currentTime = 0;
             $this.removeClass('playing');
             clearInterval(myinterval)
         } else {
-            audio.play();
             $this.addClass('playing');
+
+            audio.play();
             myinterval = setInterval(function () {
                 pulsing();
             }, pulse)
@@ -24,7 +71,8 @@ $(document).ready(() => {
                 $this.removeClass('pulse');
             }, pulse - 100)
         }
+
     })
-
-
 })
+
+
